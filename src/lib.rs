f953b03f66fdb0761290d9af108dd940bf01708a
symphonee <sandra.traven@gmail.com>
2020-09-05 16:18:53 +0200

    Add dependency ggez version 0.5. Create window and draw text. Create lib.rs file to hold game logic separetely as suggested by the rust doc the book ch12-03.

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use ggez::*;
+
+pub struct State {}
+
+impl ggez::event::EventHandler for State {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+
+        draw_text(ctx, "Hej Björn!", 200.0, 200.0)?;
+        draw_text(ctx, "Let's do this!", 200.0, 250.0)?;
+        graphics::present(ctx)
+    }
+}
+
+fn draw_text(ctx: &mut Context, message: &str, x: f32, y: f32) -> GameResult<()> {
+    let font = graphics::Font::default();
+    let mut text = graphics::Text::new(message);
+    text.set_font(font, graphics::Scale::uniform(40.0));
+    text.set_bounds(nalgebra::Point2::new(400.0, 100.0), graphics::Align::Center);
+    graphics::draw(
+        ctx,
+        &text,
+        graphics::DrawParam::default()
+            .dest(nalgebra::Point2::new(x, y))
+            .color(graphics::WHITE),
+    )?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use breakout_rust::State;
+use ggez::*;
+
 fn main() {
-    println!("Hello, world!");
+    let state = &mut State {};
+    let c = conf::Conf::new();
+    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("breakout", "bjandra")
+        .conf(c)
+        .build()
+        .unwrap();
+    event::run(ctx, event_loop, state).unwrap();
 }
