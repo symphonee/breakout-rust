fadadb4942dcb9a51669837e3c12253ed091cfda
symphonee <sandra.traven@gmail.com>
2020-09-08 09:52:32 +0200

    Add time, ball position and ball velocity data to State. Update position and draw position in the EventHandler.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,31 @@
 use ggez::*;
 
-pub struct State {}
+pub struct State {
+    dt: std::time::Duration,
+    pub ball_position: nalgebra::Point2<f32>,
+    pub ball_velocity: nalgebra::Vector2<f32>,
+}
+
+impl State {
+    pub fn new(
+        ball_position: nalgebra::Point2<f32>,
+        ball_velocity: nalgebra::Vector2<f32>,
+    ) -> State {
+        State {
+            dt: std::time::Duration::new(0, 0),
+            ball_position,
+            ball_velocity,
+        }
+    }
+}
 
 impl ggez::event::EventHandler for State {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        self.dt = timer::delta(ctx);
+        self.ball_position = nalgebra::Point2::new(
+            self.ball_position.x + self.ball_velocity.x * self.dt.as_secs_f32(),
+            self.ball_position.y + self.ball_velocity.y * self.dt.as_secs_f32(),
+        );
         Ok(())
     }
 
@@ -15,6 +37,7 @@ impl ggez::event::EventHandler for State {
         draw_text(ctx, "-------------------", 200.0, 300.0)?;
         draw_text(ctx, "Hell yes, let's go! :D", 200.0, 350.0)?;
         draw_text(ctx, "This is so exciting!!", 200.0, 400.0)?;
+        draw_ball(ctx, &self.ball_position)?;
         graphics::present(ctx)
     }
 }
@@ -31,5 +54,19 @@ fn draw_text(ctx: &mut Context, message: &str, x: f32, y: f32) -> GameResult<()>
             .dest(nalgebra::Point2::new(x, y))
             .color(graphics::WHITE),
     )?;
+    Ok(())
+}
+
+fn draw_ball(ctx: &mut Context, ball_position: &nalgebra::Point2<f32>) -> GameResult<()> {
+    let circle = graphics::Mesh::new_circle(
+        ctx,
+        graphics::DrawMode::fill(),
+        *ball_position,
+        10.0,
+        2.0,
+        graphics::WHITE,
+    )
+    .unwrap();
+    graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use breakout_rust::State;
 use ggez::*;
 
 fn main() {
-    let state = &mut State {};
+    let state = &mut State::new(
+        nalgebra::Point2::new(200.0, 200.0),
+        nalgebra::Vector2::new(0.0, -30.0),
+    );
     let c = conf::Conf::new();
     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("breakout", "bjandra")
         .conf(c)
